ffc8cc9215e5c5951d4ad916155f806fb8bbc6b0
Federico Funes <federicofunes92@gmail.com>
2019-12-30 12:15:07 -0300

    avances

--- src/index.js
+++ src/index.js
@@ -1,19 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import CssBaseline from '@material-ui/core/CssBaseline';
-import { BrowserRouter, Switch } from 'react-router-dom';
+import { HashRouter, Switch } from 'react-router-dom';
 import App from './App';
+import 'typeface-roboto';
 
 function Root() {
 
     return (
         <>
             <CssBaseline />
-            <BrowserRouter>
+            <HashRouter>
                 <Switch>
                     <App />
                 </Switch>
-            </BrowserRouter>
+            </HashRouter>
         </>
     );
 }
